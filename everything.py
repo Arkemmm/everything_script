bba27afb96b01e454c9c9ef31c450ee248ff5867
Arkemmm <112651933+Arkemmm@users.noreply.github.com>
2023-02-21 20:27:19 +0100

    Add files via upload

---
+++ everything.py
@@ -0,0 +1,13 @@
+#Affichage des formules
+print("Groupe de relations")
+print("1. Calculs d'énergie")
+print("2. Thermodynamique")
+print("3. Mécanique")
+print("4. Courrants et signaux")
+print("5. Géométrie")
+
+groupe = float(input("Indiquez votre choix :"))
+if groupe == 1:
+    print("1. Energie cinétique")
+    print("2. Energie potentiel de pesanteur")
+    print("3. Energie hydrolique")
