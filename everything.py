12104c5f64dfa1c3b9d0462012a62aa7475a0f89
Arkemmm <112651933+Arkemmm@users.noreply.github.com>
2023-02-22 11:05:15 +0100

    Update everything.py

--- everything.py
+++ everything.py
@@ -26,5 +26,36 @@ if groupe == 1:
             EC_vitesse= float(input("Vitesse de l'objet :"))
             Etot = (EC_masse / 2) * (EC_vitesse ** 2)
             print("L'énergie cinétique E = {:.2f} J".format(Etot))
-            
+        elif energie_cinetique == 2:
+            EC_vitesse= float(input("Vitesse de l'objet :"))
+            EC_energie= float(input("Energie cinétique de l'objet :"))
+            masse = (2 * EC_energie) / (EC_vitesse ** 2)
+            print("La masse de l'objet est de {:.2f} kg".format(masse))
+
+        elif energie_cinetique == 3:
+            EC_masse= float(input("Masse de l'objet :"))
+            EC_energie= float(input("Energie cinétique de l'objet :"))
+            vitesse = ((2 * EC_energie) / EC_masse) ** 0.5
+            print("La vitesse de l'objet est de {:.2f} m/s".format(vitesse))
+
+    elif energie == 2:
+        print("Quelle variable cherchez-vous ?")
+        print("1. E (en Joule)")
+        print("2. M (en kg)")
+        print("4. H (en m)")
+        energie_potentielle = float(input("Indiquez votre choix :"))
+        if energie_potentielle == 1:
+            EP_masse= float(input("Masse de l'objet :"))
+            EP_hauteur= float(input("Hauteur de l'objet :"))
+            EP_gravite= float(input("Valeur de la gravité :"))
+            Epot = EP_masse * EP_gravite * EP_hauteur
+            print("L'énergie potentielle Ep = {:.2f} J".format(Epot))
+
+        elif energie_potentielle == 2:
+            EP_hauteur= float(input("Hauteur de l'objet :"))
+            EP_gravite= float(input("Valeur de la gravité :"))
+            EP_energie= float(input("Energie potentielle de l'objet :"))
+            masse = EP_energie / (EP_gravite * EP_hauteur)
+            print("La masse de lo'bjet est de {:.2f} kg".format(masse))
+
 
